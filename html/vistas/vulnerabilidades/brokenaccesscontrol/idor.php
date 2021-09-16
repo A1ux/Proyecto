9767cdb47d1280b5d0aad5ffac181ac6b30ed150
A1ux <ruanocristian97@gmail.com>
2021-09-15 22:37:43 -0600

    brokentodos

---
+++ html/vistas/vulnerabilidades/brokenaccesscontrol/idor.php
@@ -0,0 +1,24 @@
+<?php include("../../header.php"); ?>
+
+<title>Insecure Direct Object Reference</title>
+
+<section id="template">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h3 class="title text-center">Insecure Direct Object Reference</h3>
+            <div class="titleHR"><span></span></div>
+          </div>
+        </div>
+
+<h2>Mis notas</h2>
+<ul>
+    <li><a href="./idor/2.txt">Nota 1</a></li>
+</ul>
+
+
+    </div> <!--/.container -->
+    </section> 
+    
+
+<?php include("../../footer.php"); ?>
